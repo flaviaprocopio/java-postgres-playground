0ef64fe31db48d071261f96957f1ff596170def5
Flávia Procopio <flavia.ayana@gmail.com>
2023-08-01 17:44:23 +0000

    criando tipos compostos de classes

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -1,41 +1,59 @@
 package com.example;
 
 class Cliente {
-    double renda;
-    char sexo;
-    int ano_nascimento;
+    private double renda;
+    private char sexo;
+    public char getSexo() {
+        return sexo;
+    }
 
+    public void setSexo(char sexo) {
+        if (sexo == 'M' || sexo =='F')
+            this.sexo = sexo;
+        else System.out.println("Sexo inválido");
+    }
+
+    public int getAno_nascimento() {
+        return ano_nascimento;
+    }
+
+    public void setAno_nascimento(int ano_nascimento) {
+        this.ano_nascimento = ano_nascimento;
+    }
+
+    private int ano_nascimento;
+
+public double getRenda() {
+    return renda;
 }
 
-double getRenda () {
-    return renda
-}
-
-void setRenda (double renda) {
-
+public void setRenda (double renda) {
+    if(renda >= 0)
+        this.renda = renda;
+    else System.out.println("A renda deve ser maior que zero!");
+    }
 }
 
 public class AppClasses {
     public static void main(String[] args) {
         System.out.println("Criando Classes");
         Cliente cliente = new Cliente();
-        cliente.renda = 10000;
-        cliente.sexo = 'M';
-        cliente.ano_nascimento= 1980;
+        cliente.setRenda(-10000);
+        cliente.setSexo('M');
+        cliente.setAno_nascimento(1980);
 
-        System.out.println("Renda: " + cliente.renda);
-        System.out.println("Sexo: " + cliente.sexo);
-        System.out.println("Ano de Nascimento: " + cliente.ano_nascimento);
+        System.out.println("Renda: " + cliente.getRenda());
+        System.out.println("Sexo: " + cliente.getSexo());
+        System.out.println("Ano de Nascimento: " + cliente.getAno_nascimento());
         System.out.println();
 
         Cliente cliente2 = new Cliente();
-        cliente2.renda = 2000;
-        cliente2.sexo = 'F';
-        cliente2.ano_nascimento= 1970;
+        cliente2.setRenda(2000);
+        cliente2.setSexo('s');
+        cliente2.setAno_nascimento(1970);
 
-        System.out.println("Renda: " + cliente2.renda);
-        System.out.println("Sexo: " + cliente2.sexo);
-        System.out.println("Ano de Nascimento: " + cliente2.ano_nascimento);
+        System.out.println("Renda: " + cliente2.getRenda());
+        System.out.println("Sexo: " + cliente2.getSexo());
+        System.out.println("Ano de Nascimento: " + cliente2.getAno_nascimento());
     }
-    
 }
