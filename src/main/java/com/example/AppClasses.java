f54f5c18d95f812800fb461929fc84b4ec138b76
Flávia Procopio <flavia.ayana@gmail.com>
2023-08-01 16:51:34 +0000

    AppClasses

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -7,11 +7,35 @@ class Cliente {
 
 }
 
+double getRenda () {
+    return renda
+}
+
+void setRenda (double renda) {
+
+}
+
 public class AppClasses {
     public static void main(String[] args) {
         System.out.println("Criando Classes");
-        Cliente cliente1 = new Cliente();
+        Cliente cliente = new Cliente();
+        cliente.renda = 10000;
+        cliente.sexo = 'M';
+        cliente.ano_nascimento= 1980;
 
+        System.out.println("Renda: " + cliente.renda);
+        System.out.println("Sexo: " + cliente.sexo);
+        System.out.println("Ano de Nascimento: " + cliente.ano_nascimento);
+        System.out.println();
+
+        Cliente cliente2 = new Cliente();
+        cliente2.renda = 2000;
+        cliente2.sexo = 'F';
+        cliente2.ano_nascimento= 1970;
+
+        System.out.println("Renda: " + cliente2.renda);
+        System.out.println("Sexo: " + cliente2.sexo);
+        System.out.println("Ano de Nascimento: " + cliente2.ano_nascimento);
     }
     
 }
