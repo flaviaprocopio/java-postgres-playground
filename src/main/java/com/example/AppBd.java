6214aaffd0fdc3972ba8d6329d5bdefc3efc2b7c
Flávia Procopio <flavia.ayana@gmail.com>
2023-08-02 19:08:59 +0000

    Descobrindo Campos de uma tabela Dinamicamente

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -17,8 +17,35 @@ public class AppBd {
         try (var conn = getConnection()){
             listarEstados(conn);
             localizarEstado(conn, "PA");
+            listarDadosTabela(conn, "produto");
         } catch (SQLException e) {
             System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());                          
+        }
+    }
+
+    private void listarDadosTabela(Connection conn, String tabela) {
+        var sql = "select * from " + tabela;
+        //System.out.println(sql);
+        try {
+            var statement = conn.createStatement();
+            var result = statement.executeQuery(sql);
+
+            var metadata = result.getMetaData();
+            int cols = metadata.getColumnCount();
+
+            for (int i = 1; i <= cols; i++) {
+                System.out.printf("%-25s | " , metadata.getColumnName(i));  
+            }
+            System.out.println();
+
+            while(result.next()){
+                for (int i = 1; i <= cols; i++) {
+                    System.out.printf("%-25s | " , result.getString(i));  
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro na execução da consulta: " + e.getMessage());
         }
     }
 
@@ -27,12 +54,13 @@ public class AppBd {
             //var sql = "Select * from estado where UF = '" + uf + "'"; //Suscetível a SQL injection
             var sql = "Select * from estado where UF = ?";
             var statement = conn.prepareStatement(sql);
-                System.out.println(sql);
+                //System.out.println(sql);
                 statement.setString(1, uf);
             var result = statement.executeQuery();
             if(result.next()){
                 System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
             }
+            System.out.println();
         } catch (SQLException e) {
             System.err.println("Erro ao tentar executar consulta SQL: " + e.getMessage());
         }
